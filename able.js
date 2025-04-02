e19a27ca4d0ca17faf727fe08634805cb16fd51d
Brian Porter <brian@bucca.com>
2025-04-02 15:05:54 -0500

    Create able.js

---
+++ able.js
@@ -0,0 +1,242 @@
+let paths = window.location.pathname.split('/')
+
+// badge -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33865.css?v=797511'; 
+  document.head.appendChild(link); 
+} 
+ 
+// banner -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33866.css?v=946671'; 
+  document.head.appendChild(link); 
+} 
+ 
+// button -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33861.css?v=98201'; 
+  document.head.appendChild(link); 
+} 
+ 
+// cell -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33867.css?v=198880'; 
+  document.head.appendChild(link); 
+} 
+ 
+// chiclet -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33868.css?v=12527'; 
+  document.head.appendChild(link); 
+} 
+ 
+// chip -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33869.css?v=854499'; 
+  document.head.appendChild(link); 
+} 
+ 
+// color -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33858.css?v=127341'; 
+  document.head.appendChild(link); 
+} 
+ 
+// custom -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33929.css?v=104607'; 
+  document.head.appendChild(link); 
+} 
+ 
+// element -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33871.css?v=332196'; 
+  document.head.appendChild(link); 
+} 
+ 
+// field -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33872.css?v=763234'; 
+  document.head.appendChild(link); 
+} 
+ 
+// fluid -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/35627.css?v=639440'; 
+  document.head.appendChild(link); 
+} 
+ 
+// global -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/35751.css?v=883969'; 
+  document.head.appendChild(link); 
+} 
+ 
+// label -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33859.css?v=652164'; 
+  document.head.appendChild(link); 
+} 
+ 
+// listitem -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33873.css?v=507311'; 
+  document.head.appendChild(link); 
+} 
+ 
+// minified -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/35739.css?v=353761'; 
+  document.head.appendChild(link); 
+} 
+ 
+// object -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33874.css?v=790075'; 
+  document.head.appendChild(link); 
+} 
+ 
+// picker -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33875.css?v=478285'; 
+  document.head.appendChild(link); 
+} 
+ 
+// rdio-cbox -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33876.css?v=418075'; 
+  document.head.appendChild(link); 
+} 
+ 
+// segbtn -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33877.css?v=252163'; 
+  document.head.appendChild(link); 
+} 
+ 
+// slider -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33878.css?v=490932'; 
+  document.head.appendChild(link); 
+} 
+ 
+// snackbar -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33879.css?v=634414'; 
+  document.head.appendChild(link); 
+} 
+ 
+// stepper -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33880.css?v=527262'; 
+  document.head.appendChild(link); 
+} 
+ 
+// tab -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33881.css?v=519177'; 
+  document.head.appendChild(link); 
+} 
+ 
+// tags -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33882.css?v=375625'; 
+  document.head.appendChild(link); 
+} 
+ 
+// text -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33863.css?v=469704'; 
+  document.head.appendChild(link); 
+} 
+ 
+// tooltip -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33883.css?v=45114'; 
+  document.head.appendChild(link); 
+} 
+ 
+// typography -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33862.css?v=181696'; 
+  document.head.appendChild(link); 
+} 
+ 
+// utility -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/33930.css?v=970847'; 
+  document.head.appendChild(link); 
+} 
+ 
+// WF Header -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/35806.css?v=110185'; 
+  document.head.appendChild(link); 
+} 
+ 
+// working-on -> 
+{ 
+  const link = document.createElement('link'); 
+  link.rel = 'stylesheet'; 
+  link.href = 'https://slater.app/13495/35606.css?v=71906'; 
+  document.head.appendChild(link); 
+} 
+ 
